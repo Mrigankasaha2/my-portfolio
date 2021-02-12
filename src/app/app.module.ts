2641adc2af1b90b483ee24b8cc36861c6df84d25
Mriganka Saha <mrigankasaha2@gmail.com>
2021-02-12 17:53:03 +0530

    Add basic component :droplet:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './Views/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MainbodyComponent } from './Views/mainbody/mainbody.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent, NavbarComponent, MainbodyComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
